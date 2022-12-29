37f8e4b23929632058d6ac9e4edc2f3b7475236f
hjpunzalan <hj.punzalan@hotmail.com>
2022-12-29 08:59:11 +0800

    Refactor app and catalog

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,26 +1,15 @@
 import { Container, CssBaseline } from "@mui/material";
-import { SetStateAction, useEffect, useState } from "react";
 import Catalog from "src/features/catalog/Catalog";
-import { storeAPI } from "src/helpers/axios";
 import Header from "./Header";
-import { Product } from "./models/product";
 
 function App() {
-	const [products, setProducts] = useState<Product[] | undefined>();
-
-	useEffect(() => {
-		storeAPI
-			.get<Product[]>("/products")
-			.then((res: { data: SetStateAction<Product[] | undefined> }) => {
-				setProducts(res.data);
-			});
-	}, []);
-
 	return (
 		<>
 			<CssBaseline />
 			<Header />
-			<Container>{products && <Catalog products={products} />}</Container>
+			<Container>
+				<Catalog />
+			</Container>
 		</>
 	);
 }
--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -1,15 +1,21 @@
 import { Button } from "@mui/material";
+import { SetStateAction, useEffect, useState } from "react";
 import { Product } from "src/app/layout/models/product";
+import { storeAPI } from "src/helpers/axios";
 import ProductList from "./ProductList";
 
-interface Props {
-	products: Product[];
-}
-
-const Catalog = ({ products }: Props) => {
+const Catalog = () => {
+	const [products, setProducts] = useState<Product[] | undefined>();
+	useEffect(() => {
+		storeAPI
+			.get<Product[]>("/products")
+			.then((res: { data: SetStateAction<Product[] | undefined> }) => {
+				setProducts(res.data);
+			});
+	}, []);
 	return (
 		<>
-			<ProductList products={products} />
+			{products && <ProductList products={products} />}
 			<Button variant="contained">Add Product</Button>
 		</>
 	);
