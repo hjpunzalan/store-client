4b2a841e134fef4ae0057372ef3b6c1dbc3cf85e
hjpunzalan <hj.punzalan@hotmail.com>
2022-12-30 10:26:59 +0800

    add product details into page content

--- src/features/catalog/ProductDetails.tsx
+++ src/features/catalog/ProductDetails.tsx
@@ -1,4 +1,12 @@
-import { Typography } from "@mui/material";
+import {
+	Divider,
+	Grid,
+	Table,
+	TableBody,
+	TableCell,
+	TableRow,
+	Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "src/app/layout/models/product";
@@ -22,8 +30,52 @@ const ProductDetails = (props: Props) => {
 	}, [id]);
 
 	if (loading) return <h3>Loading...</h3>;
-	if (product) return <Typography variant="h2">{product.name}</Typography>;
-	else return <h3>Product not found!</h3>;
+	if (!product) return <h3>Product not found!</h3>;
+	return (
+		<Grid container spacing={6}>
+			<Grid item xs={6}>
+				<img
+					src={product.pictureUrl}
+					alt={product.name}
+					style={{ width: "100%" }}
+				/>
+			</Grid>
+			<Grid item xs={6}>
+				<Typography variant="h3">{product.name}</Typography>
+				<Divider sx={{ mb: 2 }} />
+				<Typography variant="h4" color="secondary">
+					{Intl.NumberFormat("en-US", {
+						style: "currency",
+						currency: "USD",
+					}).format(product.price / 100)}
+				</Typography>
+				<Table>
+					<TableBody>
+						<TableRow>
+							<TableCell>Name</TableCell>
+							<TableCell>{product.name}</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell>Description</TableCell>
+							<TableCell>{product.description}</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell>Types</TableCell>
+							<TableCell>{product.type}</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell>Brand</TableCell>
+							<TableCell>{product.brand}</TableCell>
+						</TableRow>
+						<TableRow>
+							<TableCell>Quantiy in stock</TableCell>
+							<TableCell>{product.quantityInStock}</TableCell>
+						</TableRow>
+					</TableBody>
+				</Table>
+			</Grid>
+		</Grid>
+	);
 };
 
 export default ProductDetails;
