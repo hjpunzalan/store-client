47eef6c01d8381c1d7871e997504ef0fc0b2c1cb
hjpunzalan <hj.punzalan@hotmail.com>
2023-01-31 23:18:31 +0800

    add counter slice and reconfigure store

--- src/app/store/configureStore.ts
+++ src/app/store/configureStore.ts
@@ -1,6 +1,21 @@
-import { createStore } from "redux";
-import counterReducer from "src/features/contact/counterReducer";
+import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { counterSlice } from 'src/features/contact/counterSlice';
 
-export function configureStore() {
-	return createStore(counterReducer);
-}
+// export function configureStore() {
+// 	return createStore(counterReducer);
+// }
+
+export const store = configureStore({
+	reducer: {
+		counter: counterSlice.reducer,
+	},
+});
+
+// types
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+// hook
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,22 @@
-import { createBrowserHistory } from "history";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { Router } from "react-router";
-import { BrowserRouter } from "react-router-dom";
-import "../src/app/layout/styles.css";
-import { StoreProvider } from "./app/context/StoreContext";
-import App from "./app/layout/App";
-import { configureStore } from "./app/store/configureStore";
-import reportWebVitals from "./reportWebVitals";
+import '../src/app/layout/styles.css';
+
+import { createBrowserHistory } from 'history';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from 'src/app/store/configureStore';
+
+import { StoreProvider } from './app/context/StoreContext';
+import App from './app/layout/App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
 
 export const history = createBrowserHistory();
-const store = configureStore();
-
 root.render(
 	<BrowserRouter>
 		<Router history={history}>
