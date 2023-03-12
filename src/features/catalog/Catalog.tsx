da477c3f5035f8f877cb6f33e29cd6d23d41a806
hjpunzalan <hj.punzalan@hotmail.com>
2023-03-12 23:09:42 +0800

    storing filter state in redux from API

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -2,17 +2,21 @@ import { Button } from "@mui/material";
 import { useEffect } from "react";
 import LoadingComponent from "src/app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "src/app/store/configureStore";
-import { fetchProductsAsync, productSelectors } from "src/features/catalog/catalogSlice";
+import { fetchFilters, fetchProductsAsync, productSelectors } from "src/features/catalog/catalogSlice";
 import ProductList from "./ProductList";
 
 const Catalog = () => {
   const products = useAppSelector(productSelectors.selectAll);
   const dispatch = useAppDispatch();
-  const { productsLoaded, status } = useAppSelector((state) => state.catalog);
+  const { productsLoaded, status, filtersLoaded } = useAppSelector((state) => state.catalog);
 
   useEffect(() => {
     if (!productsLoaded) dispatch(fetchProductsAsync());
   }, [dispatch, productsLoaded]);
+
+  useEffect(() => {
+    if (!filtersLoaded) dispatch(fetchFilters());
+  }, [dispatch, filtersLoaded]);
 
   if (status.includes("pending")) return <LoadingComponent message="Loading Products..." />;
 
