a5c75e87619f8eee8fd8dab297d5250041c91526
hjpunzalan <hj.punzalan@hotmail.com>
2023-03-13 23:43:50 +0800

    adding product params to redux state

--- src/app/layout/models/product.ts
+++ src/app/layout/models/product.ts
@@ -1,10 +1,19 @@
 export interface Product {
-	id: number;
-	brand: string;
-	description: string;
-	name: string;
-	pictureUrl: string;
-	price: number;
-	quantityInStock: number;
-	type: string;
+  id: number;
+  brand: string;
+  description: string;
+  name: string;
+  pictureUrl: string;
+  price: number;
+  quantityInStock: number;
+  type: string;
+}
+
+export interface ProductParams {
+  orderBy: string;
+  searchTerm?: string;
+  types?: string[];
+  brands?: string[];
+  pageNumber: number;
+  pageSize: number;
 }
