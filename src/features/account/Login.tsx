6c90b22eec9b7154ae3a63f70e36f352484bfa3e
hjpunzalan <hj.punzalan@hotmail.com>
2023-11-03 00:34:41 +0800

    using react hook form

--- src/features/account/Login.tsx
+++ src/features/account/Login.tsx
@@ -1,8 +1,8 @@
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { LoadingButton } from "@mui/lab";
 import { Paper } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Checkbox from "@mui/material/Checkbox";
 import Container from "@mui/material/Container";
 import FormControlLabel from "@mui/material/FormControlLabel";
@@ -11,7 +11,7 @@ import MuiLink from "@mui/material/Link";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
-import { useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import agent from "src/app/api/agent";
 
@@ -19,20 +19,14 @@ import agent from "src/app/api/agent";
 const defaultTheme = createTheme();
 
 export default function Login() {
-  const [values, setValues] = useState({
-    username: "",
-    password: "",
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // Submit to server
-    agent.Account.login(values);
-  };
-
-  function handleInputChange(event: any) {
-    const { name, value } = event.target;
-    setValues({ ...values, [name]: value });
+  async function submitForm(data: FieldValues) {
+    await agent.Account.login(data);
   }
 
   return (
@@ -48,35 +42,31 @@ export default function Login() {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <Box component="form" onSubmit={handleSubmit(submitForm)} noValidate sx={{ mt: 1 }}>
           <TextField
             margin="normal"
             required
             fullWidth
             id="username"
             label="Username"
-            name="username"
             autoComplete="username"
             autoFocus
-            value={values.username}
-            onChange={handleInputChange}
+            {...register("username")}
           />
           <TextField
             margin="normal"
             required
             fullWidth
-            name="password"
             label="Password"
             type="password"
             id="password"
-            value={values.password}
             autoComplete="current-password"
-            onChange={handleInputChange}
+            {...register("password")}
           />
           <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
-          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+          <LoadingButton loading={isSubmitting} type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
             Sign In
-          </Button>
+          </LoadingButton>
           <Grid container>
             <Grid item>
               <Link component={(props) => <MuiLink variant="body2" {...props} />} to="/register">
